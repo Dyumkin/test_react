c83aa581cddbc144af0eaf1b6bbfa8892396d085
Yury Dyumkin <dyumkin@gmail.com>
2016-11-21 21:35:02 +0300

    init api env

---
+++ api/config/app.js
@@ -0,0 +1,79 @@
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compress from 'compression';
+import methodOverride from 'method-override';
+import cors from 'cors';
+import expressWinston from 'express-winston';
+import helmet from 'helmet';
+import winstonInstance from './winston';
+import config from './env';
+import path from 'path';
+import middlewares from '../middlewares';
+import container from '../components/container';
+
+const app = express(),
+      errors = container.component('errors');
+
+if (config.env === 'development') {
+    app.use(logger('dev'));
+}
+
+// view engine setup
+app.set('views', path.join(`../${__dirname}`, 'views')); //todo fix
+app.set('view engine', 'jade');
+
+// parse body params and attache them to req.body
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({extended: false, limit: '50mb'}));
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(cookieParser());
+app.use(compress()); //not required
+app.use(methodOverride()); //not required
+
+// secure apps by setting various HTTP headers
+app.use(helmet());
+
+// middlewares
+app.use(middlewares.http);
+
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors());
+
+if (app.get('env') !== 'production') {
+    // api documentation
+    app.use('/apidoc', express.static(`../${__dirname}/public/documentation/api`)); //todo fix
+}
+
+// enable detailed API logging in dev env
+if (config.env === 'development') {
+    expressWinston.requestWhitelist.push('body');
+    expressWinston.responseWhitelist.push('body');
+    app.use(expressWinston.logger({
+        winstonInstance,
+        meta: true, // optional: log meta data about request (defaults to true)
+        msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms',
+        colorStatus: true // Color the status code (default green, 3XX cyan, 4XX yellow, 5XX red).
+    }));
+}
+
+// log error in winston transports except when executing test suite
+if (config.env !== 'test') {
+    app.use(expressWinston.errorLogger({
+        winstonInstance
+    }));
+}
+
+// controllers
+app.use(require('../controllers'));
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => next(new errors.NotFound()));
+
+// error handler
+app.use(container.service('error-handler'));
+
+export default app;
